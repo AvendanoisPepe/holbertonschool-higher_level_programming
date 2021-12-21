aaea2044da1947c3876e7b89e30e43a7f0a45488
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-12-21 18:04:39 +0000

    correcciones

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    siguiente = ((number * -1) % 10) * -1
+    num = ((number * -1) % 10) * -1
 else:
-    siguiente = number % 10
+    num = number % 10
 
-if siguiente == 0:
-    print("Last digit of {} is {} and is 0".format(number, siguiente))
-elif siguiente > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, siguiente))
-elif siguiente < 6 and siguiente != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, siguiente))
+if num == 0:
+    print("Last digit of {} is {} and is 0".format(number, num))
+elif num > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, num))
+elif num < 6 and num != 0:
+    print("Last digit of {} is {} and is less\
+    than 6 and not 0".format(number, num))
     
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -11,3 +11,5 @@
 4-print_hexa.py - Usando un ciclo for recorremos un rango de 0 a 99 en el cual imprimos su version decimal y si version hexadecimal usando hex() para convertir el numero.
 
 5-print_comb2.py - Usando un ciclo for recorremos un rango de 0 a 99 en el cual tenemos una condicional donde verificos el ultimo digito para imprimir diferente, todos los numeros se imprimen de dos digitos.
+
+6-print_comb3.py -
