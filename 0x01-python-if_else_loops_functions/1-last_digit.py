93b357a362b245478ea4f1708af638fbfd0dd4fa
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-12-21 17:33:57 +0000

    one point

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,6 +5,7 @@ if number < 0:
     siguiente = ((number * -1) % 10) * -1
 else:
     siguiente = number % 10
+
 if siguiente == 0:
     print("Last digit of {} is {} and is 0".format(number, siguiente))
 elif siguiente > 5:
