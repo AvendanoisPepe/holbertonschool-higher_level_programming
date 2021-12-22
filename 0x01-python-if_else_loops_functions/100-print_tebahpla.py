926ca5a66c98ede4d8a16bb763aac51c33573190
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-12-22 03:08:17 +0000

    thirth  point

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for numerito in range(ord("z"), ord("a"), -1):
-    if numerito % 2 == 0:
-        print("{}".format(chr(numerito)), end="")
-    else:
-        print("{}".format(chr(numerito - 32)), end="")
+for numerito in range(ord('z'), ord('a') -1, -1):
+    if numerito % 2 != 0:
+        numerito = numerito - 32
+    print("{:c}".format(numerito), end="")
