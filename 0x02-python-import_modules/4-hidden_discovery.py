bec1e336b12d5610d8c66c241299555a117b3b8f
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-12-27 20:12:21 +0000

    fifth  point

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from hidden_4 import *
     arreglo = dir()
     for iterador in range(0, len(arreglo)):
-        if arreglo[iterador] != "__":
+        if arreglo[iterador][0:2] != "__":
             print("{}".format(arreglo[iterador]))
