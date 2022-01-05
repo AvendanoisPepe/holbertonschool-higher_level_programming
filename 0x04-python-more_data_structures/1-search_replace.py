c0333d440b08e08af3f2eaf05be4b720c913e664
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-01-05 17:34:21 +0000

    correciones del 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if search in my_list:
-        new_list = my_list.copy()
-        j = 0
-        for i in my_list:
-            if i == search:
-                new_list[j] = replace
-            j += 1
+        acumulador = 0
+        listita = my_list.copy()
+        for iterador in my_list:
+            if iterador == search:
+                listita[acumulador] = replace
+            acumulador = acumulador + 1
     else:
-        new_list = my_list
-    return new_list
+        listita = my_lists
+    return listita
