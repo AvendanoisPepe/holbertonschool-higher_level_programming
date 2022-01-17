6059187d4dd2f70dc331326d8cf542004cd23475
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-01-17 17:16:18 +0000

    second point

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    contador  = 0
-    for iterador in range (0, x):
+    contador = 0
+    for iterador in range(0, x):
         try:
             print("{:d}".format(my_list[iterador]), end="")
             contador = contador + 1
