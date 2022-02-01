3750b115366e9c1e41db57234cb2dcea156b47c4
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-02-01 01:36:08 +0000

    corecciones de 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Modulo BaseGeometry que contiene
-un metodo area"""
+""" Geometry Integer validator
+Contains the class BaseGeometry.
+    """
 
 
 class BaseGeometry():
-    """Clase BaseGeometry"""
+    """ Define public attribute area and integer validator  """
 
     def area(self):
-        """Genera una excepcion"""
+        """ Raises an Exception """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validamos el valor en el metodo publico"""
+        """ Validates if value is integer """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
