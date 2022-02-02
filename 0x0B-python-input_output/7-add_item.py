fbcc74ad07604c83b52c1e2023903288e13087dc
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-02-02 01:34:55 +0000

    seventh point

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 try:
     lista = load_from_json_file("add_item.json")
-except:
+except Exception:
     lista = []
 
 argc = len(sys.argv)
