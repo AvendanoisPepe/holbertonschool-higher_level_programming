4712c901f94f3842e170df822c93aa1ed4ffd4a5
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-04-04 12:21:51 -0500

    del 5 al 9

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-    for (let iterador = 0; iterador < process.argv[2]; iterador++) {
-        console.log('C is fun');
-    }
+  for (let iterador = 0; iterador < process.argv[2]; iterador++) {
+    console.log('C is fun');
+  }
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-    for (let iterador = 0; iterador < process.argv[2]; iterador++) {
-        console.log('X'.repeat(process.argv[2]));
-    }
+  for (let iterador = 0; iterador < process.argv[2]; iterador++) {
+    console.log('X'.repeat(process.argv[2]));
+  }
 }
