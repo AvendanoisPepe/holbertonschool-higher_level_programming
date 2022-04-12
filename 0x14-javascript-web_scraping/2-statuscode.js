f861d8a1c9750043f381d071285d1883799ba769
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-04-12 09:25:34 -0500

    pe

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], function (err, response) {
+const urlToGet = process.argv[2];
+
+request(urlToGet, function (err, response) {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
-
