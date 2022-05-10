f3971265dda0d983c369becc71d7bd8843290622
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-10 13:20:13 -0500

    pep

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,10 +10,11 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cu = db.cursor()
-    cu.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY states.id ASC".
+    cu.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".
                format(argv[4]))
     rows = cu.fetchall()
     for fila in rows:
-        print(fila)
+        if fila[1] == argv[4]:
+            print(fila)
     cu.close()
     db.close()
