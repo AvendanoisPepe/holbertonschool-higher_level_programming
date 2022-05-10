6bb1c29a2cffb1a09cc2567484de92ec4f8619a4
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-10 16:52:44 -0500

    pepe

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Clase base
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.schema import ForeignKey
+from model_state import Base, State
+Base = declarative_base()
+
+
+class City(Base):
+    """Hereda de base"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(100), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
