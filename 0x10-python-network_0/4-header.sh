0e98820cbedca63b7602712695d73a93e6863c60
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-18 17:40:17 -0500

    pepe

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Muestra el cuerpo de la respuesta de una solicitud curl con encabezado
-curl -sX "GET" "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sX "GET" -H "X-HolbertonSchool-User-Id: 98" "$1"
