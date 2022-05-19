2cb43d11cdf9eec2eef6d9500f0d7d0c88a2efa6
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-19 13:11:48 -0500

    pepe

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Escriba una secuencia de comandos de Python que tome una URL y un correo electrónico, envíe una solicitud POST a la URL pasada con el correo electrónico como parámetro y muestre el cuerpo de la respuesta (decodificado en utf-8)
+"""Escriba una secuencia de comandos de Python que tome una URL
+y un correo electrónico, envíe una solicitud POST a la URL pasada con
+el correo electrónico como parámetro y muestre el cuerpo de la
+respuesta (decodificado en utf-8)
 """
 import urllib.request
 import urllib.parse
@@ -11,7 +14,6 @@ if __name__ == "__main__":
     email = sys.argv[2]
     value = {'email': email}
     data = urllib.parse.urlencode(value)
-    data = data.encode('utf-8')
     with urllib.request.urlopen(url, data) as response:
         answer = response.read().decode('utf-8')
     print(answer)
