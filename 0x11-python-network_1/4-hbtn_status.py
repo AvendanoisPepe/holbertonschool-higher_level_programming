46afc1f5eea8d2f318c7a417cb9c0bddcf18e974
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-19 13:42:30 -0500

    pepe

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Escriba un script de Python que obtenga https://intranet.hbtn.io/status
 """
+import requests
 
 if __name__ == "__main__":
-    import requests
-    
+
     url = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('\t - type: {}'.format(type(url.text)))
-    print('\t - content: {}'.format(url.text))
+    print('\t- type: {}'.format(type(url.text)))
+    print('\t- content: {}'.format(url.text))
