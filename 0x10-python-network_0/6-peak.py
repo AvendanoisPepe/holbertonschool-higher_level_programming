927422bb4a368cd0ded54b834199f1ec10edc6e8
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-18 19:56:46 -0500

    pepe

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,7 @@
 def find_peak(list_of_integers):
     """Devuelve un número máximo de una lista"""
 
-    if list_of_integers:
-        list_of_integers.sort(reverse=True)
-        return list_of_integers[0]
+    if len(list_of_integers) < 1:
+        return None
+    list_of_integers.sort()
+    return list_of_integers[-1]
