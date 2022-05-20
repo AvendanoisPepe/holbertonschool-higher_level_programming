7a19911e6db972694f17de639922488e29eaf25b
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-19 23:49:41 -0500

    pepep

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-"""Escriba un script de Python que reciba una carta y envíe
-una solicitud POST a http://0.0.0.0:5000/search_user con la carta
-como parámetro.
+"""Escriba un script de Python que tome sus credenciales de GitHub
+(nombre de usuario y contraseña) y use la API de GitHub para mostrar
+su identificación
 """
 import requests
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        value = ""
-    else:
-        value = sys.argv[1]
-    url = requests.post('http://0.0.0.0:5000/search_user', data={'q': value})
-    try:
-        urls = url.json()
-        if len(urls) == 0:
-            print("No result")
-        else:
-            print("[{}] {}".format(urls["id"], urls["name"]))
-    except Exception as error:
-        print("Not a valid JSON")
+    requests = requests.get('https://docs.github.com/en/rest/users', auth=(sys.argv[1], sys.argv[2]))
+    print(requests.json().get('id'))
