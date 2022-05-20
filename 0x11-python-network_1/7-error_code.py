ea799c164b3c8e0ba4639c3b23da27428ac4a6b3
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-19 22:57:49 -0500

    pepe

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    r = requests.get(argv[1])
-    status = r.status_code
-    print(r.text) if status < 400 else print(
-        "Error code: {}".format(r.status_code))
+    url = requests.get(sys.argv[1])
+    err = url.status_code
+    print(url.text) if err < 400 else print(
+        "Error code: {}".format(url.status_code))
